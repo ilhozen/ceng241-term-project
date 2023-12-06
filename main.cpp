b1deacfad7c3657cc55754cd67258f301c978597
ilayda1110 <c2211012@student.cankaya.edu.tr>
2023-12-06 17:37:45 +0300

    added Item class and its sub class Box

--- main.cpp
+++ main.cpp
@@ -1,14 +1,36 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
 
+class Item //Class for the items we will use
+{
+	private:
+		string Name;
+	public:
+		void setName(string name) { Name = name; }  // !!! set name de koydum bu classlara ki değerleri alabilelim
+		string getName() { return Name; } 
+};
+
+class Box: public Item //Sub-class of Item: Box
+{
+	private:
+		char Symbol; //each of box has its own symbol
+	public:
+		void setSymbol(char symbol) { Symbol = symbol; }
+		char getSymbol() { return Symbol; }
+};
+
 
 int main() {
-    cout << "Hello, World!" << endl;
     
-    cout << "ceng241" << endl;
-    
-    cout << "our project "<<endl;
+    Box boxes[10];
+    char j = 'A';
+    for (int i = 0; i < 10; i++) //assigning symbols to boxes
+    {
+    	boxes[i].setSymbol(j);
+    	j++;
+	}
     
     
     
