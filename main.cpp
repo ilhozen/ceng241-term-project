32690ca8788e676ada755834124404b973d89992
İlhan Özen <62931699+ilhozen@users.noreply.github.com>
2023-12-09 00:54:50 +0300

    added experiment materials class and related function

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include <string>
 
 using namespace std;
+const string COMPOUNDCOLOR = "TEMP_COLOR";
 
 class Item //Class for the items we will use
 {
@@ -21,6 +22,30 @@ class Box: public Item //Sub-class of Item: Box
 		char getSymbol() { return Symbol; }
 };
 
+class ExperimentMaterial: public Item
+{
+private:
+    bool used;
+public:
+    bool isUsed(){ return used; }
+    void setUsed(int c){
+        if (c == 1){
+            used = true;
+        }else{
+            used = false;
+        }
+    }
+};
+
+void combine(ExperimentMaterial mat1, ExperimentMaterial mat2){
+    if(mat1.getName()== "TEMP_NAME" && mat2.getName()=="TEMP_NAME" || mat2.getName()== "TEMP_NAME" && mat1.getName()=="TEMP_NAME"){
+        cout << "You have combined " << mat1.getName() << "and " << mat2.getName() << ". It turned " << COMPOUNDCOLOR << endl;
+    }else{
+        cout << "This doesn't seem like the correct formula, I should check the notes to be sure.." << endl;
+        mat1.setUsed(0);
+        mat2.setUsed(0);
+    }
+}
 
 int main() {
     
@@ -31,7 +56,7 @@ int main() {
     	boxes[i].setSymbol(j);
     	j++;
 	}
-    
+
     
     
     return 0;
