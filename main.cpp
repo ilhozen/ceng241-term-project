49300daa592a49bb75cc45a483b97d4bc4d30fb6
pnrszn <psuzen10@gmail.com>
2023-12-17 00:33:55 +0300

    added "Page" and "Plants" subclasses

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+#include <vector>
 
 using namespace std;
 const string COMPOUNDCOLOR = "TEMP_COLOR";
@@ -9,14 +10,14 @@ class Item //Class for the items we will use
 	private:
 		string Name;
 	public:
-		void setName(string name) { Name = name; }  // !!! set name de koydum bu classlara ki değerleri alabilelim
+		void setName(string name) { Name = name; }
 		string getName() { return Name; } 
 };
 
-class Box: public Item //Sub-class of Item: Box
+class Box: public Item 
 {
 	private:
-		char Symbol; //each of box has its own symbol
+		char Symbol;
 	public:
 		void setSymbol(char symbol) { Symbol = symbol; }
 		char getSymbol() { return Symbol; }
@@ -46,13 +47,54 @@ void combine(ExperimentMaterial mat1, ExperimentMaterial mat2){
         mat2.setUsed(0);
     }
 }
+
+class Page: public Item
+{
+	private:
+		vector<string>text;
+		bool read;
+	public:
+		void printNotebook(){
+			for(int i = 0 ; i < text.size() ; i++){
+				cout << text[i] << endl;			
+			}
+		}
+		void isRead(int Read){
+			if(Read == 1){
+				read = true;
+			}
+			else{
+				read = false;
+			}
+		}
+		void setText(string Text){text.push_back(Text);}
+};
+	
+class Plants: public Item
+{
+	private:
+		int leafNum, date;
+		string color, name;
+	public:
+		void setColor(string Color){ color = Color; }
+		void setDate(int Date){ date = Date; }
+		void setleafNum(int LeafNum){ leafNum = LeafNum; }
+		void setName(string Name){ name = Name; }
+		
+		void printInfo(){
+			cout << "Name : " << name << endl;
+			cout << "Planting Date : " << date << endl;
+			cout << "Color : " << color << endl;
+			cout << "Number of Leaves : " << leafNum << endl;
+		}
+};
+
 class Lock{
 	protected:
 		int code;
 		bool opened;
-		};
 	public:
-		void setcode(code){this->code=code}
+		void setcode(int code){this->code=code}
 		string getcode() { return code;} 
 		void opened(int A){
         if (A == 1){
@@ -60,7 +102,6 @@ class Lock{
         }else{
             opened = false;
         }
-        };
 		int check_code(string entered_code){
 			if(entered_code== *******){
 				return (opened(1));
@@ -69,6 +110,7 @@ class Lock{
 				return opened(0);
 		}
 };
+
 class DoorLock:public Lock{
     private:
 		string symbol_code;
@@ -85,7 +127,7 @@ class ComputerLock:public Lock{
 		char getsymbol_code() { return symbol_code };
 		
 	
-};
+};*/
 
 int main() {
     
@@ -97,7 +139,19 @@ int main() {
     	j++;
 	}
 
-    
-    
-    return 0;
+	string labEquipment[10] = {"Glasses" , "Coats" , "Flask" , "Gloves" , "Test tube" , "Microscope" , "Funnel" , "Beaker" , "Magnet" , "Pipette"};
+	string compounds[5] = {"yellow + red" , "blue + yellow" , "blue + red" , "green + blue" , "green + red"};
+	
+	
+
+	Page controlNotebook;
+	Page notebookOfCompounds;
+	
+	for(int i = 0 ; i < 10 ; i++){
+		controlNotebook.setText(labEquipment[i]);
+	}
+	
+	for(int i = 0 ; i < 5 ; i++){
+		notebookOfCompounds.setText(compounds[i]);
+	}
 }
